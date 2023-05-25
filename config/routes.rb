3aa988771816b8fd37078d207fc412511de8db30
Alejandro Leon <79841123+Alexleon2411@users.noreply.github.com>
2023-05-25 20:31:28 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "lists", to: "lists#index", as: :list
+  get "lists/new", to: "lists#new"
+  get "lists/:id", to: "lists#show"
+  post "lists", to: "lists#create"
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  delete "bookmarks/:id", to: "bookmarks#destroy"
 end
